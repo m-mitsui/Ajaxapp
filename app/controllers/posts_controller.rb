65ee4c767bdef368fc840b6ea4a31317f9d99f56
m-mitsui <paperclip321@icloud.com>
2020-07-31 18:25:21 +0900

    フロント側の記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,22 @@
 class PostsController < ApplicationController
- def index
-    @posts = Post.all.order(id: "DESC")
- end
+  def index
+      @posts = Post.all.order(id: "DESC")
+  end
  
- def create
-   Post.create(content: params[:content])
+  def create
+    Post.create(content: params[:content])
    redirect_to action: :index #トップページへ戻るアクション
   end
 
   def checked
-    post = Post.find(params[:id]) #レコードを取得する記述
+    post = Post.find(params[:id])
     if post.checked then
-      post.update(checked: false) 
+      post.update(checked: false)
     else
       post.update(checked: true)
     end
-# post.checkedで既読したかどうかを条件分岐している
+
     item = Post.find(params[:id])
-    render json: { post: item 
-      # json形式でchecked.jsに変換している
+    render json: { post: item }
   end
 end
